daf4d7bbdfc65004721f1bd4a3d75a47a5a55344
Mineo Okuda <okuda@willstyle.co.jp>
2020-06-07 23:42:00 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['prefix' => 'admin'], function(){
+
+    //home
+    Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+    //login logout   
+    Route::get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\Auth\LoginController@login')->name('admin.login');
+    Route::post('logout', 'Admin\Auth\LoginController@logout')->name('admin.logout');
+    //register
+    Route::get('register', 'Admin\Auth\RegisterController@showRegisterForm')->name('admin.register');
+    Route::post('register', 'Admin\Auth\RegisterController@register')->name('admin.register');
+    
+});
+    
+
+Route::get('/home', 'HomeController@index')->name('home');
